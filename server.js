7db7c5f5aafc584637cd2cf96dfba54897a6e13f
yair-roshal <yair7705778@gmail.com>
2023-02-13 20:04:09 +0200

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config()
 const token = process.env.TELEGRAM_BOT_TOKEN
 
 const bot = new TelegramBot(token, { polling: true })
-const YOUR_CHAT_ID = 386212074
+const chatIdAdmin = 386212074
 
 const dictionaryText = require('./data/dictionaryText.js')
 let date = new Date()
@@ -19,37 +19,90 @@ let date = new Date()
 let dictionary = dictionaryText.split(/\r?\n/).filter(Boolean)
 // console.log('dictionary', dictionary)
 
-const addWordsKeyboard = {
+function openKlava(chatId) {
+    bot.sendMessage(chatId, 'Клавиатура открыта', {
+        reply_markup: {
+            keyboard: [
+                [
+                    {
+                        text: 'Классика',
+                    },
+                    {
+                        text: 'Закрыть',
+                    },
+                ],
+                [
+                    {
+                        text: 'Заказать разработку бота',
+                        request_contact: true,
+                    },
+                ],
+                [
+                    {
+                        text: 'Про автора',
+                    },
+                ],
+            ],
+            one_time_keyboard: true,
+        },
+    })
+}
+
+bot.onText(/\/keyboard/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Alternative keybaord layout', MainMenu)
+})
+
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id
+    bot.sendMessage(chatId, 'Приветик, ' + msg.chat.first_name + '!')
+    openKlava(chatId)
+})
+
+bot.on('callback_query', (query) => {
+    if (query.data === 'development') {
+        bot.sendMessage(chatIdAdmin, 'development menu', MainMenu)
+    }
+    if (query.data === 'lifestyle') {
+        bot.sendMessage(chatIdAdmin, 'lifestyle menu', MainMenu)
+    }
+    if (query.data === 'other') {
+        bot.sendMessage(chatIdAdmin, 'other menu', MainMenu)
+    }
+})
+
+const MainMenu = {
     reply_markup: {
-        inline_keyboard: [
+        keyboard: [
             [
                 {
-                    text: 'Add words',
-                    callback_data: 'add_words',
+                    text: 'Development',
+                    callback_data: 'development',
+                },
+                {
+                    text: 'Lifestyle',
+                    callback_data: 'lifestyle',
+                },
+                {
+                    text: 'Other',
+                    callback_data: 'other',
                 },
             ],
         ],
+        // keyboard: [['Sample text', 'Second sample'], ['Keyboard'], ["I'm robot"]],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+        force_reply: true,
     },
 }
-
-bot.on('callback_query', (query) => {
-    if (query.data === 'add_words') {
-        bot.sendMessage(
-            YOUR_CHAT_ID,
-            'Please send me the words, separated by newlines.',
-            addWordsKeyboard,
-        )
-    }
-})
 
 //отправив список слов добавляем их в словарь
 bot.on('message', (msg) => {
     console.log('msg.text===', msg.text)
     if (msg.text == '/start') {
-        bot.sendMessage(YOUR_CHAT_ID, `Server-Bot successfully started  `)
+        bot.sendMessage(chatIdAdmin, `Server-Bot successfully started  `)
     } else if (!dictionary.includes(msg.text) && msg.text !== '/start') {
         dictionary = dictionary.concat(msg.text.split(/\r?\n/))
-        bot.sendMessage(YOUR_CHAT_ID, `Successfully added "${msg.text}" to the dictionary.`)
+        bot.sendMessage(chatIdAdmin, `Successfully added "${msg.text}" to the dictionary.`)
     }
 })
 
@@ -59,10 +112,10 @@ function sendRandomWord() {
     const wordEng = word.split('-')[0].trim()
     // say.speak(wordEng)
 
-    bot.sendMessage(YOUR_CHAT_ID, `${randomIndex + 1}. ${word}`)
+    bot.sendMessage(chatIdAdmin, `${randomIndex + 1}. ${word}`)
 
     date = new Date()
-    console.log(date.toLocaleTimeString(), `--- ${randomIndex + 1}.  ${word}`)
+    console.log(date.toLocaleTimeString(), `--- ${randomIndex + 1}.${word}`)
 }
 const ms = 1000
 const sec = 60
