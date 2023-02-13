4b0a4e26fc09490885598af9c495ab7479729125
yair-roshal <yair7705778@gmail.com>
2023-02-13 11:28:28 +0200

    123

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api')
-const token = '6209657165:AAGifVt0gs11dAHGQ2Uz3yAz_s5u0st5arY'
 const bot = new TelegramBot(token, { polling: true })
 const YOUR_CHAT_ID = 386212074
 
-let dictionary=[]
-const dictionaryText = require('./data/dictionaryText.js')
+// const token = '6209657165:AAGifVt0gs11dAHGQ2Uz3yAz_s5u0st5arY'
+const dotenv = require('dotenv');
+dotenv.config();
+const token = process.env.TELEGRAM_BOT_TOKEN;
+ 
+ const dictionaryText = require('./data/dictionaryText.js')
 
- dictionary = dictionary.concat(dictionaryText.split(/\r?\n/))
+
+// text = [address, city, state, zip].filter(Boolean).join(", ");
+
+// let dic =dictionaryText.split(/\r?\n/).filter(Boolean).join(", ")
+let dictionary = dictionary.concat(dictionaryText.split(/\r?\n/)).filter(Boolean)
 console.log('dictionary', dictionary)
 
 const addWordsKeyboard = {
@@ -39,4 +46,4 @@ function sendRandomWord() {
     bot.sendMessage(YOUR_CHAT_ID, word)
 }
 
-setInterval(sendRandomWord, 6 * 1 * 1000)
+setInterval(sendRandomWord, 30 * 60 * 1000)
