89255a877008bb2a28590adce8fe441e15ee2691
yair-roshal <yair7705778@gmail.com>
2023-02-13 11:31:23 +0200

    123

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
 const TelegramBot = require('node-telegram-bot-api')
 // const token = '6209657165:AAGifVt0gs11dAHGQ2Uz3yAz_s5u0st5arY'
-const dotenv = require('dotenv');
-dotenv.config();
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const dotenv = require('dotenv')
+dotenv.config()
+const token = process.env.TELEGRAM_BOT_TOKEN
 
 const bot = new TelegramBot(token, { polling: true })
 const YOUR_CHAT_ID = 386212074
 
-
- 
- const dictionaryText = require('./data/dictionaryText.js')
-
+const dictionaryText = require('./data/dictionaryText.js')
 
 // text = [address, city, state, zip].filter(Boolean).join(", ");
 
 // let dic =dictionaryText.split(/\r?\n/).filter(Boolean).join(", ")
-let dictionary = dictionary.concat(dictionaryText.split(/\r?\n/)).filter(Boolean)
+
+let dictionary = dictionaryText.split(/\r?\n/).filter(Boolean)
 console.log('dictionary', dictionary)
 
 const addWordsKeyboard = {
-  reply_markup: {
-    inline_keyboard: [[
-      {
-        text: 'Add words',
-        callback_data: 'add_words'
-      }
-    ]]
-  }
+    reply_markup: {
+        inline_keyboard: [
+            [
+                {
+                    text: 'Add words',
+                    callback_data: 'add_words',
+                },
+            ],
+        ],
+    },
 }
 
-bot.on('callback_query', query => {
-  if (query.data === 'add_words') {
-    bot.sendMessage(YOUR_CHAT_ID, 'Please send me the words, separated by newlines.', addWordsKeyboard)
-  }
+bot.on('callback_query', (query) => {
+    if (query.data === 'add_words') {
+        bot.sendMessage(
+            YOUR_CHAT_ID,
+            'Please send me the words, separated by newlines.',
+            addWordsKeyboard,
+        )
+    }
 })
 
-bot.on('message', msg => {
-  if (!dictionary.includes(msg.text)) {
-    dictionary = dictionary.concat(msg.text.split(/\r?\n/))
-    bot.sendMessage(YOUR_CHAT_ID, `Successfully added "${msg.text}" to the dictionary.`)
-  }
+bot.on('message', (msg) => {
+    if (!dictionary.includes(msg.text)) {
+        dictionary = dictionary.concat(msg.text.split(/\r?\n/))
+        bot.sendMessage(YOUR_CHAT_ID, `Successfully added "${msg.text}" to the dictionary.`)
+    }
 })
 
 function sendRandomWord() {
